386169fbd58da8f4db6639896b025154f148be9e
aleadbetterdev <anthony.leadbetter@silverhorsetech.com>
2024-04-16 23:13:48 +0800

    type made, list called and functioned 32.28

--- 2024-react/src/App.tsx
+++ 2024-react/src/App.tsx
@@ -1,5 +1,7 @@
 // no imports needed, no return block needed
 
+import { dummyData } from "./data/todos"
+
 function App() {
 
   return (
@@ -7,6 +9,15 @@ function App() {
       <h1 className="font-bold text-3xl text-center">
         Your to-do list
       </h1>
+      <div>
+        <div>
+          {dummyData.map(todo => (
+            <p key={todo.id}>
+              {todo.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
---
+++ 2024-react/src/data/todos.ts
@@ -0,0 +1,20 @@
+// get the type of interface from file
+import { Todo } from "../types/todo";
+// The data types are inferred from above //
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "First little title",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "And another woo!",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Here's the third",
+        completed: false,
+    },
+];
