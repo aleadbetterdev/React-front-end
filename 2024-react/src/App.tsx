c6f1c8217ac4a1180184464560e3ccce9db97f9b
aleadbetterdev <anthony.leadbetter@silverhorsetech.com>
2024-04-19 01:05:10 +0800

    Created component to use in app.tsx, created react prompt that works

--- 2024-react/src/App.tsx
+++ 2024-react/src/App.tsx
@@ -1,20 +1,19 @@
 // no imports needed, no return block needed
 
+import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
 
 function App() {
 
   return (
-    <main className="py-10 h-screen">
+    <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">
         Your to-do list
       </h1>
-      <div>
-        <div>
-          {dummyData.map(todo => (
-            <p key={todo.id}>
-              {todo.title}
-            </p>
+      <div className="max-w-lg mx-auto bg-slate-100 rounder-md p-5">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <TodoItem todo={todo}/>
           ))}
         </div>
       </div>
