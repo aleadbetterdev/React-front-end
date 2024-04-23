c1468b5ccd0415ed2467712502ff2f4ca423e45a
aleadbetterdev <anthony.leadbetter@silverhorsetech.com>
2024-04-24 00:07:59 +0800

    Created form, made form adds stay in state

--- 2024-react/src/components/AddTodoForm.tsx
+++ 2024-react/src/components/AddTodoForm.tsx
@@ -1,7 +1,25 @@
-export default function AddTodoForm(){
+import { useState } from "react"
+
+interface AddTodoFormProps {
+    onSubmit: (title: string) => void;
+}
+
+export default function AddTodoForm({onSubmit}: AddTodoFormProps) {
+    const [input, setInput] = useState("");
+
+        function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+            e.preventDefault();
+
+            if (!input.trim()) return;
+
+            onSubmit(input);
+            setInput("");
+        }
     return(
-        <form className="flex">
+        <form className="flex" onSubmit={handleSubmit}>
             <input 
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
                 placeholder="What needs to be done?"
                 className="rounded-s-md grow border border-gray-400 p-2"
             />
